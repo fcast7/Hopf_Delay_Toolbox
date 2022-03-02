8ffe11a56e61418414c32fd6c5d4eba68e9376c3
fcast7 <74235438+fcast7@users.noreply.github.com>
2022-03-02 10:59:08 +0100

    Update and rename Hopf_delays_feature_extraction.m to hopf_delays_feature_extraction.m

--- hopf_delays_feature_extraction.m
+++ hopf_delays_feature_extraction.m
@@ -1,38 +1,70 @@
-function Hopf_delays_feature_extraction
+function hopf_delays_feature_extraction(a,dt_save,MD,expK,C) 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  Whole-brain Hopf Bifurcation Model with time delay:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% Whole-brain Hopf Bifurcation Model with time delay%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%             
+%      This function loads simulated time series (Zsave - from hopf_delays_simu) and measures 
+%      Spectral and global dynamical features on simulated data 
 %
-%                   Spectral and dynamical features on simulated data
+%  - Input: Define Simulation Parameter 
+%  
+%     
+%     a:       bifurcation parameter 
+%     dt_save: resolution or downsampling 
+%     MD:      range of delays in ms 
+%     expK:    range of couplings (expK)
+%     C:       structural connectome, nodes_parcellation_nsubj (i.e. AAL90n32s) 
 %
-%  1) Input: Define Simulation Parameter (a, K, MD) 
-%
-%  2) Input:  Load simulated time series (Zsave);
-%     Output: Spectral features of simulated time series in the Whole-Brain Hopf Model
+%   
+%  - Output: Spectral features of simulated time series in the Whole-Brain Hopf Model
 %             to save as Model_Spectral_Features.mat (uncomment fo measuring also Spectral
-%            Entropy and Lempel-Ziv Complexity)
+%             Entropy and Lempel-Ziv Complexity)
 %
-%  Joana Cabral 2017 joana.cabral@psych.ox.ac.uk
-%  Francesca Castaldo francesca.castaldo.20@ucl.ac.uk
-%
+%  Examples: 
+%  32AAL, -5 simu: Hopf_delays_feature_extraction(-5,2e-3,0:1:20,-1:0.1:1.7,AAL90n32s)
+%  985AAL, -5 simu: Hopf_delays_feature_extraction(-5,2e-3,0:1:20,-1:0.1:1.5,AAL90n985s)
+%  
+%  Demo:
+%  Hopf_delays_feature_extraction(-5,2e-3,0:1:5,-1:0.1:1.7,AAL90n32s)
+%  
+%  Code by Francesca Castaldo 2022 francesca.castaldo.20@ucl.ac.uk
+%  Adapted from Joana Cabral 2017 joana.cabral@psych.ox.ac.uk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-addpath(genpath('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu'));
-% 1) Define simulation Parameters
 
-a=-5; %bifurcation parameter of your simulations 
-N=90; %nodes parcellation
-dt_save=2e-3;
-f=40; %Hz intrinsic frequency 
-MD=0:1:10; % Reange of Mean Delay in ms
+%% 1) Define your own path for accessing simulations
+if a==-5
+    if C==AAL90n32s
+        cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu\32AAL\a_neg5')
+        disp(['Running for' num2str(a) '90AAL32'])
+    elseif C==AAL90n985s
+        cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu\985AAL\a_neg5')
+        disp(['Running for' num2str(a) '90AAL985'])
+    elseif C==SHEAF200n32s
+        cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[SHEAFER]\Simulations')
+        disp(['Running for' num2str(a) '200SHEAF32'])
+    end
+
+elseif a==-0.2
+    if C==AAL90n32s
+    cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu\32AAL\a_neg02')
+    disp(['Running for' num2str(a) '90AAL32'])
+    end
+    
+elseif a==-0.05
+    if C==AAL90n32s
+        cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu\32AAL\a_neg005\Simulations')
+        disp(['Running for' num2str(a) '90AAL32'])
+    end
+end
+
+% Define simulation Parameters Scaling
 MD=MD.*1e-3; % Mean Delay in seconds
+K=10.^(expK);
 fbins=1000;
 freqZ = (0:fbins-1)/(dt_save*fbins);
 
-expK=1.8:0.1:2;
-% [rows, columns] = size(expK);
-% expK = reshape(sort(expK(:), 'descend'), [columns, rows])';
-K=10.^(expK);
-
+% Inizialize 
 Sync                 = zeros(length(K),length(MD));
 Meta                 = zeros(length(K),length(MD));
 PeakFMean            = zeros(length(K),length(MD));
@@ -40,7 +72,7 @@ PeakFGlobal          = zeros(length(K),length(MD));
 % SpecEntropy_Global = zeros(length(K), length(MD));
 % lzcomplexity       = zeros(length(K), length(MD));
 
-% 2) Generate spectral features given simulated time series
+%%  2) Generate spectral features given simulated time series
 
 % figure('color', 'w') %only if one wants to see the simulations in the whole parameter space
 
@@ -56,13 +88,11 @@ for g=1:length(K)
         if numel(ind_p)
             K_label(ind_p)='p';
         end
-        
-        %Load your simulations (depending on the parameter you would like
-        %to investigate)
-%         cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu')
-%         load(['Hopf_Simu_K1E' K_label '_MD' num2str(MD*1e3)],'Zsave','dt_save')
-        load(['a_Remote_K1E' K_label '_MD_' num2str(md*1e3) 'a' num2str(a)],'Zsave') %for cluster new simulations
-        
+       
+        % load simulations 
+        load(['a_Remote_K1E' K_label '_MD_' num2str(md*1e3) 'a' num2str(a)],'Zsave')
+
+
         % Detect the peak Frequency in the Fourier Transform of all areas
         Fourier_Complex = fft(Zsave,fbins,2); %% Fourier of Z (complex) in 2nd dimension
        
@@ -74,7 +104,7 @@ for g=1:length(K)
         % Band-pass filter the signals Z around the peak frequency of the
         % ensemble
        
-        for n=1:90
+        for n=1:size(Zsave,1)
             Zb(n,:)=bandpasshopf(Zsave(n,:),[max(0.1,freqZ(Imax)-1) freqZ(Imax)+1],1/dt_save);
             Zb(n,:)=angle(hilbert(Zb(n,:)));
             
@@ -85,7 +115,7 @@ for g=1:length(K)
         Meta(g,d)=std(OP); %  how much OP fluctuates in time (std of OP)
         
         % Probability distribution (total sum is 1)
-        PSD_G=Fourier_Global/sum(Fourier_Global);
+%         PSD_G=Fourier_Global/sum(Fourier_Global);
         
         
         % Average of Node PSD
@@ -94,7 +124,7 @@ for g=1:length(K)
         PeakFMean(g,d)=freqZ(Imax);
         
         % Probability distribution (total sum is 1)
-        PSD_M=Fourier_Mean/sum(Fourier_Mean);
+%         PSD_M=Fourier_Mean/sum(Fourier_Mean);
         
         %Spectral Entropy method 1
 %         PSD=PSD_G/sum(PSD_G+1e-12);
@@ -124,5 +154,7 @@ for g=1:length(K)
         
     end
 end
-cd('C:\Users\fcast\OneDrive - University College London\CLUSTER\PROJECT[AAL]\New_Simu')
+
+%Save 
 save([('Model_Spectral_Features')],'PeakFGlobal', 'PeakFMean','Sync', 'Meta') %,'lzcomplexity','SpecEntropy_Global')
+
